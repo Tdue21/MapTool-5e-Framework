5c9e8aca6981809f573c86e1fac11c017d5354cb
tdue21 <tho.due@gmail.com>
2024-08-20 22:27:59 +0200

    Did a little work

--- source/5e-framework/library/public/scripts/createTable.js
+++ source/5e-framework/library/public/scripts/createTable.js
@@ -1,29 +1,31 @@
 "use strict";
 
-try {
-    const args = MTScript.getMTScriptCallingArgs()[0];
-    const data = JSON.parse(atob(args));
-
-    MTScript.setVariable("tableName", data.tableName);
-    MTScript.setVariable("visible", data.visible ? 1 : 0);
-    MTScript.setVariable("rollable", data.rollable ? 1 : 0);
-    MTScript.setVariable("assetId", getAssetId(data.imageUrl));
-    MTScript.setVariable("tableRoll", data.tableRoll);
-
-    MTScript.evalMacro(`
-        [h:createTable(tableName, visible, rollable, assetId)]
-        [h:setTableRoll(tableName, tableRoll)]
-    `);
-
-    for (const element of data.entries) {
+function createTable(tableName, visible, rollable, tableRoll, qqqqimageUrl) {
+    try {
         MTScript.setVariable("tableName", tableName);
-        MTScript.setVariable("rangeStart", element.start);
-        MTScript.setVariable("rangeEnd", element.end);
-        MTScript.setVariable("result", element.value);
-        MTScript.setVariable("assetId", getAssetId(element.imageUrl));
+        MTScript.setVariable("visible", visible ? 1 : 0);
+        MTScript.setVariable("rollable", rollable ? 1 : 0);
+        MTScript.setVariable("assetId", getAssetId(imageUrl));
+        MTScript.setVariable("tableRoll", tableRoll);
 
-        MTScript.evalMacro(`[h:addTableEntry(tableName, rangeStart, rangeEnd, result, assetId)]`);
+        MTScript.evalMacro(`[h:createTable(tableName, visible, rollable, assetId)]
+[h:setTableRoll(tableName, tableRoll)]`);
+
+        for (const element of data.entries) {
+            MTScript.setVariable("tableName", tableName);
+            MTScript.setVariable("rangeStart", element.start);
+            MTScript.setVariable("rangeEnd", element.end);
+            MTScript.setVariable("result", element.value);
+            MTScript.setVariable("assetId", getAssetId(element.imageUrl));
+
+            MTScript.evalMacro(`[h:addTableEntry(tableName, rangeStart, rangeEnd, result, assetId)]`);
+        }
+    } catch (e) {
+        MapTool.chat.broadcast("createTable: " + e + "\n" + e.stack);
     }
-} catch (e) {
-    MapTool.chat.broadcast("createTable: " + e + "\n" + e.stack);
+}
+MTScript.registerMacro("createTable", createTable);
+
+function createMap() {
+    
 }
